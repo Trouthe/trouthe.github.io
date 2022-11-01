5a7d8328b4913d5c11a41f8551e89e3fae5cc99e
Trouthe <anthony.hajjar00@gmail.com>
2022-11-01 18:29:26 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-// const pg = document.createElement('span');
-// pg.innerHTML = `
-//     <p>This is an added paragraph</p>
-// `;
 var cons = document.getElementById("console");
 var button = document.getElementById("addAboutMe");
+var langs = new Array("Angular.js", "TypeScript", "JS", "php", "React.js", "🤯");
+var i = 0;
 button === null || button === void 0 ? void 0 : button.addEventListener('click', function handleClick(event) {
     var pg = document.createElement("p");
-    pg.textContent = "This is a new paragraph";
-    cons === null || cons === void 0 ? void 0 : cons.appendChild(pg);
+    var pg2 = document.createElement("p");
+    var test = document.createElement("p");
+    test.textContent = "TEST";
+    pg.textContent = "A passionate Computer Science🚀student eager to grow and learn along the way 💪🏻";
+    pg2.textContent = "Currently learning the following: ";
+    cons === null || cons === void 0 ? void 0 : cons.append(pg);
+    setTimeout(function () {
+        cons === null || cons === void 0 ? void 0 : cons.appendChild(pg2);
+        setTimeout(function () {
+            languages();
+        }, 750);
+    }, 1000);
 });
+function languages() {
+    var lng = document.createElement("p");
+    if (i < langs.length) {
+        lng.textContent = langs[i];
+        cons === null || cons === void 0 ? void 0 : cons.appendChild(lng);
+        i++;
+        setTimeout(languages, 750);
+    }
+}
--- app.ts
+++ app.ts
@@ -1,10 +1,39 @@
-
-
 const cons = document.getElementById("console");
 const button = document.getElementById("addAboutMe");
+var langs = new Array ("Angular.js", "TypeScript", "JS", "php", "React.js", "🤯");
+var i = 0;
 
 button?.addEventListener('click', function handleClick (event) {
     const pg = document.createElement("p");
-    pg.textContent = "This is a new paragraph";
-    cons?.appendChild(pg);
+    const pg2 = document.createElement("p");
+
+    const test = document.createElement("p");
+    test.textContent = "TEST";
+
+    pg.textContent = "A passionate Computer Science🚀student eager to grow and learn along the way 💪🏻";
+    pg2.textContent = "Currently learning the following: ";
+
+    cons?.append(pg);
+
+    setTimeout(() => {
+        cons?.appendChild(pg2);
+
+        setTimeout(() => {
+            languages();
+        }, 750);
+    }, 1000);
 });
+
+function languages () {
+    const lng = document.createElement("p");
+
+    if (i < langs.length) {
+        lng.textContent = langs[i];
+
+        cons?.appendChild(lng);
+
+        i++;
+        setTimeout(languages, 750);
+    }
+
+}
